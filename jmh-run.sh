40138e1dc704a28a0865ad3facb7ed593ad42d94
Dmitry Komanov <dmitryk@wix.com>
2016-11-20 23:36:50 +0200

    Move jmh-result.json to docs folder

--- jmh-run.sh
+++ jmh-run.sh
@@ -5,14 +5,10 @@ ec() {
     $*
 }
 
-# expecting a gh-pages symlink in a root directory
-#GH_PAGES_PATH=`readlink gh-pages`
-# ensure that git repository exists there
-#ec git -C $GH_PAGES_PATH status > /dev/null
-
 ec mvn clean install
 
 ec java -jar scala-string-format-test/target/benchmarks.jar -rf json -rff jmh-result.json > jmh.log
 
-#ec mv jmh-result.json $GH_PAGES_PATH
-#echo "Don't forget to push gh-pages!"
+ec mv jmh-result.json docs/
+
+echo "Don't forget to push docs!"
