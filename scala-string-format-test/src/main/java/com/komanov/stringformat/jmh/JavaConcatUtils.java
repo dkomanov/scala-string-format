81643297a329b7cf14c4fb5c6e8a2003dc40dd83
Dmitry Komanov <dmitryk@wix.com>
2016-11-26 21:19:25 +0200

    Remove unused JavaConcatUtils

--- scala-string-format-test/src/main/java/com/komanov/stringformat/jmh/JavaConcatUtils.java
+++
@@ -1,8 +0,0 @@
-package com.komanov.stringformat.jmh;
-
-public class JavaConcatUtils {
-
-    public static String javaConcat(int value1, String value2, Object nullObject) {
-        return value1 + "a" + value2 + "b" + value2 + nullObject;
-    }
-}
