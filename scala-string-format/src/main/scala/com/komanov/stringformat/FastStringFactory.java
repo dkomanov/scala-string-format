e1ad4038b4e169ed561b998bc2cce9867bc06e08
Dmitry Komanov <dmitryk@wix.com>
2016-12-03 17:57:28 +0200

    Refactor, get field directly, avoiding getter (performance is the same)

--- scala-string-format/src/main/scala/com/komanov/stringformat/FastStringFactory.java
+++ scala-string-format/src/main/scala/com/komanov/stringformat/FastStringFactory.java
@@ -3,20 +3,23 @@ package com.komanov.stringformat;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
+import java.lang.reflect.Field;
 
 public class FastStringFactory {
 
-    private static MethodHandle getValue = getValueHandler();
-    private static MethodHandle newString = getNewStringHandler();
+    private static final MethodHandle stringBuilderValueGetter = getValueHandler();
+    private static final MethodHandle newString = getNewStringHandler();
 
     public static String fastNewString(StringBuilder sb) throws Throwable {
         if (sb.capacity() != sb.length()) {
             throw new IllegalArgumentException("Expected filled StringBuilder!");
         }
 
-        char[] chars = (char[]) getValue.invoke(sb);
-        return fastNewString(chars);
+        return fastNewString(getValue(sb));
+    }
+
+    public static char[] getValue(StringBuilder sb) throws Throwable {
+        return (char[]) stringBuilderValueGetter.invoke(sb);
     }
 
     public static String fastNewString(char[] chars) throws Throwable {
@@ -25,13 +28,18 @@ public class FastStringFactory {
 
     private static MethodHandle getValueHandler() {
         try {
-            Method method = StringBuilder.class.getSuperclass().getDeclaredMethod("getValue");
-            method.setAccessible(true);
-            return MethodHandles.lookup().unreflect(method);
+            Field field = getValueField();
+            return MethodHandles.lookup().unreflectGetter(field);
         }
         catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Field getValueField() throws NoSuchFieldException {
+        Field field = StringBuilder.class.getSuperclass().getDeclaredField("value");
+        field.setAccessible(true);
+        return field;
     }
 
     private static MethodHandle getNewStringHandler() {
